63a6337c7f7fce087942b3daa2eba1beeca290e2
chan-afk <54665416+chan-afk@users.noreply.github.com>
2019-08-29 12:39:23 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FVb29_750tnEGN-TLoZ82F5rR14TNGt26wA3v5tFgFY/edit#gid=0';
